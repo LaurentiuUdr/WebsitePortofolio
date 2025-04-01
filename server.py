4d1694c195d0edd9d841f5efcd35ddfc46539509
Laurentiu <ulaurentiu@gmail.com>
2025-04-01 21:55:30 +0300

    Create basic menu with template. I learned how Jinja work

--- server.py
+++ server.py
@@ -1,23 +1,24 @@
 from flask import Flask
 from flask import render_template
 
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template('template.html', page='Home')
+    return render_template('pages/index.html', page='Home')
 
-@app.route("/portofolio")
-def portofolio():
-    return render_template('template.html', page='Portofolio')
-
-@app.route("/resume")
+@app.route('/resume')
 def resume():
-    return render_template('template.html', page='Resume')
+    return render_template('pages/resume.html', page='Resume')
 
-@app.route("/contact")
+@app.route('/portofolio')
+def portofolio():
+    return render_template('pages/portofolio.html', page='Portofolio')
+
+@app.route('/contact')
 def contact():
-    return render_template('template.html', page='Contact')
+    return render_template('pages/contact.html', page='Contact')
 
 if __name__ == "__main_":
     app.run()

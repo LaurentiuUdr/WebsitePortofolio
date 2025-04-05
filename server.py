2ca6649e7683fa118e31ae3b71a99b811d1c565f
Laurentiu <ulaurentiu@gmail.com>
2025-04-05 09:57:21 +0300

    recreate routes

--- server.py
+++ server.py
@@ -1,24 +1,27 @@
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('pages/index.html', page='Home')
+PAGES_PATH = 'pages'
+pages = [
+    {"endpoint": "index", "url": "/", "template": f"{PAGES_PATH}/index.html", "title": "Home"},
+    {"endpoint": "resume", "url": "/resume", "template": f"{PAGES_PATH}/resume.html", "title": "Resume"},
+    {"endpoint": "portfolio", "url": "/portfolio", "template": f"{PAGES_PATH}/portfolio.html", "title": "Portfolio"},
+    {"endpoint": "contact", "url": "/contact", "template": f"{PAGES_PATH}/contact.html", "title": "Contact"},
+]
 
-@app.route('/resume')
-def resume():
-    return render_template('pages/resume.html', page='Resume')
+for page in pages:
+    def make_view(template, title):
+        def view():
+            return render_template(template, page=title)
+        return view
 
-@app.route('/portofolio')
-def portofolio():
-    return render_template('pages/portofolio.html', page='Portofolio')
-
-@app.route('/contact')
-def contact():
-    return render_template('pages/contact.html', page='Contact')
+    app.add_url_rule(
+        rule=page["url"],
+        endpoint=page["endpoint"],
+        view_func=make_view(page["template"], page["title"]),
+        methods=["GET"]
+    )
 
 if __name__ == "__main_":
     app.run()
